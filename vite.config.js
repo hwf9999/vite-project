34b4706e2b5253af2227ad464a8f439483c9371e
何文飞 <hewenfei@yolanda.hk>
2025-05-22 19:52:49 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { checkVersion } from "./plugins/vitePluginCheckVersion";
 
+// @ai-start
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(),checkVersion()],
 })
+// @ai-end
